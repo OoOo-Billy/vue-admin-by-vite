ed28c85e1e82ade94c1dd3ca89fbab96d4d693b4
闭雷宇 <bi.leiyu@xinxindigits.com>
2021-06-24 18:31:57 +0800

    typescript

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import styleImport from 'vite-plugin-style-import';
+
+const alias: Record<string, string> = {
+  '@': path.resolve(__dirname, '.', 'src'),
+  '~deps': path.resolve(__dirname, '.', 'node_modules'),
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias,
+  },
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: name => {
+            return `element-plus/lib/theme-chalk/${name}.css`;
+          },
+          resolveComponent: name => {
+            return `element-plus/lib/${name}`;
+          },
+        },
+      ],
+    }),
+  ],
+});
