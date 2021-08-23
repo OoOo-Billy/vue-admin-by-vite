4b6182c7d9c3930ff2f41cb1f7caf76a75ae71cb
OoOo-Billy <1403809057@qq.com>
2021-08-23 12:41:09 +0800

    test: ✅ 开发页测试登入登出交互

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,5 +1,18 @@
 import { defineComponent } from 'vue';
+import { useUserStore } from '@/store/modules/user';
 
-const Test = defineComponent(() => () => <div>i'm tsx sfc.</div>);
+const Test = defineComponent(() => {
+  const store = useUserStore();
+
+  return () => (
+    <div>
+      i'm tsx sfc.
+      <el-button onClick={() => store.login({ id: 'admin', pwd: 'admin' })}>
+        login
+      </el-button>
+      <el-button onClick={() => store.logout()}>logout</el-button>
+    </div>
+  );
+});
 
 export default Test;
